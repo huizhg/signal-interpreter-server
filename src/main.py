2131b5866c2658986dc99145c3729a2593b6b808
Hui Zhang <hui.zhang.umu@outlook.com>
2021-05-26 23:21:06 +0200

    Finish assignment 1 in week 1

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.routes import signal_interpreter_app
+
+
+def run():
+    signal_interpreter_app.run()
+
+
+if __name__ == "__main__":
+    run()
---
+++ src/routes.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+
+signal_interpreter_app = Flask(__name__)
+
+
+@signal_interpreter_app.route("/", methods=["POST"])
+def mirror_data():
+    data = request.get_json()
+    return data
