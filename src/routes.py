1d2b5aea17a5b26cbd7961fdad982b8ed0b73ace
Pontus Sandh <pontus.sandh@consid.se>
2021-06-10 13:20:20 +0200

    changed jsonparser data

--- src/routes.py
+++ src/routes.py
@@ -10,6 +10,8 @@ signal_interpreter_app = Flask(__name__)
 def interpret_signal():
     """interpret the signal"""
     data = request.get_json()
-    json_parser.data = data
+    # we do not want to overload the existing data in the json_parser
+    # when we call get_signal_title from postman, we compare it with our existing "database"
+    # json_parser.data = data 
     signal_title = json_parser.get_signal_title(data["signal"])
     return jsonify(signal_title)
